ac12c9292e674a8bdaf97bea1595d71222b97195
JIMm2527 <jimmymh87@gmail.com>
2022-10-19 17:39:30 -0500

    Update Program.cs

--- DataIntegratorASC/Program.cs
+++ DataIntegratorASC/Program.cs
@@ -7,56 +7,70 @@
 using DataIntegratorASC.Bussiness;
 using DataIntegratorASC.Clases;
 using System;
+using System.Threading;
 
 namespace DataIntegratorASC
 {
     public class Program
     {
+        private static Mutex mutex = null;
         public static void Main(string[] args)
         {
-        try
-        {
-            DateTime dateTime = new DateTime();
-            Program.LoadInitialValues();
+            const string appName = "DataIntegratorASC";
+            bool createdNew;
 
-                MyGlobals.sStepLog = "Catálogos";
-                new Catalogos().Import();
+            mutex = new Mutex(true, appName, out createdNew);
 
-                MyGlobals.sStepLog = "WorkOrders";
-                Utils.GuardarBitacora("Inicia:" + MyGlobals.sStepLog);
-                new OrdenTrabajoBO().Import();
-
-                MyGlobals.sStepLog = "SalesOrders";
-                Utils.GuardarBitacora("Inicia:" + MyGlobals.sStepLog);
-                new Pedidos().Import();
-
-                MyGlobals.sStepLog = "PurchaseOrders";
-                Utils.GuardarBitacora("Inicia:" + MyGlobals.sStepLog);
-                new PurchaseOrders().Import();
-
-                Utils.GuardarBitacora("Total de tiempo: " + (DateTime.Now - dateTime).ToString());
-            }
-        catch (Exception ex)
-        {
-            if (MyGlobals.oCompany.Connected)
+            if (!createdNew)
             {
-              MyGlobals.oCompany.Disconnect();
+                return;
             }
-            Utils.GuardarBitacora("Error en paso " + MyGlobals.sStepLog + ": " + ex.Message);
+            else
+            {
+                try
+                {
+                    DateTime dateTime = new DateTime();
+                    Program.LoadInitialValues();
+
+                    MyGlobals.sStepLog = "Catálogos";
+                    new Catalogos().Import();
+
+                    MyGlobals.sStepLog = "WorkOrders";
+                    Utils.GuardarBitacora("Inicia:" + MyGlobals.sStepLog);
+                    new OrdenTrabajoBO().Import();
+
+                    MyGlobals.sStepLog = "SalesOrders";
+                    Utils.GuardarBitacora("Inicia:" + MyGlobals.sStepLog);
+                    new Pedidos().Import();
+
+                    MyGlobals.sStepLog = "PurchaseOrders";
+                    Utils.GuardarBitacora("Inicia:" + MyGlobals.sStepLog);
+                    new PurchaseOrders().Import();
+
+                    Utils.GuardarBitacora("Total de tiempo: " + (DateTime.Now - dateTime).ToString());
+                }
+                catch (Exception ex)
+                {
+                    if (MyGlobals.oCompany.Connected)
+                    {
+                        MyGlobals.oCompany.Disconnect();
+                    }
+                    Utils.GuardarBitacora("Error en paso " + MyGlobals.sStepLog + ": " + ex.Message);
+                }
+            }
+        }
+
+        private static void LoadInitialValues()
+        {
+          try
+          {
+            MyGlobals.sStepLog = "Carga valores iniciales";
+            Utils.GetLoadInitialValues();
+          }
+          catch (Exception ex)
+          {
+            throw ex;
+          }
         }
     }
-
-    private static void LoadInitialValues()
-    {
-      try
-      {
-        MyGlobals.sStepLog = "Carga valores iniciales";
-        Utils.GetLoadInitialValues();
-      }
-      catch (Exception ex)
-      {
-        throw ex;
-      }
-    }
-  }
 }
